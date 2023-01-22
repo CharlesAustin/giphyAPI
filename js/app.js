f29959f2bef32aa426f944d59c6681e13c41d8c2
Austin <austinatk22@gmail.com>
2023-01-22 14:35:02 -0500

    Refactor fetch using async/await. Remove variables from global namespace.

--- js/app.js
+++ js/app.js
@@ -1,24 +1,27 @@
-const getGif = document.querySelector("#getGif");
-const gifContainer = document.querySelector(".gifContainer");
-const myKey = "Ik2y6SA8vp6BDUyFHAH8wOjGYHI8hptc";
-const iframeOptions = {
-    
-} 
-
-getGif.addEventListener("click", () => {
+const fetchGif = async () => {
+    const gifContainer = document.querySelector(".gifContainer");
+    const myKey = "Ik2y6SA8vp6BDUyFHAH8wOjGYHI8hptc";
+    const url = `https://api.giphy.com/v1/gifs/random?api_key=${myKey}`;
     gifContainer.innerHTML = "";
     gifContainer.setAttribute("aria-busy", true);
-    fetch(`https://api.giphy.com/v1/gifs/random?api_key=${myKey}`)
-    .then((response) => response.json())
-    .then((response) => {
+
+    try {
+        const response = await fetch(url);
+        const resolved = await response.json();
         const iframe = document.createElement("iframe");
-        iframe.src = response.data.embed_url;
-        iframe.width = response.data.images.original.width;
-        iframe.height = response.data.images.original.height;
+
+        iframe.src = resolved.data.embed_url;
+        iframe.width = resolved.data.images.original.width;
+        iframe.height = resolved.data.images.original.height;
         iframe.setAttribute("allowFullScreen", "");
         gifContainer.removeAttribute("aria-busy");
         gifContainer.append(iframe);
-    })
-    .catch((error) => console.log(error))
-})
+    } catch (e) {
+        gifContainer.setAttribute("aria-busy", false);
+        gifContainer.append(`Error retrieving gif.`);
+    }
+};
 
+getGif.addEventListener("click", () => {
+    fetchGif();
+});
